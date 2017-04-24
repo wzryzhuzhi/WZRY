64ec4b0e5efe1525132fba581310af5b450c3af3
wswzsxb <719163371@qq.com>
2017-04-24 22:02:11 +0800

    '熊斌修改第四次社区over'

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ angular.module("wzry",['ngWaterfall','ui.router','angularCSS','homeModule','matc
 
 ])
 .config(function($stateProvider,$urlRouterProvider){
-	$urlRouterProvider.otherwise("/home");
+	$urlRouterProvider.otherwise('/home');
+	console.log(1111111111)
 })
 
 /*1.首页-英雄列表*/
@@ -21,27 +22,9 @@ angular.module("wzry",['ngWaterfall','ui.router','angularCSS','homeModule','matc
 	})
 	.controller('heroscontrol',['$scope','$http',function($scope,$http){
 		$http.get("components/heros/heroList.json").success(function(res){
-//			$scope.arrherosort=[];
-//			$scope.arrheroall=[];
-//			$scope.arrHeros=res.data
-//			for(var i=0;i<$scope.arrHeros.length;i++){
-//				$scope.arrheroall.push($scope.arrHeros[i].heroST)
-//			};
-//			alert(2);
-//			for(var j=0;j<$scope.arrheroall.length;j++){
-//				if($scope.arrheroall.indexOf($scope.arrheroall[j])==-1){
-//					alert(1)
-//					$scope.arrherosort.push($scope.arrheroall[j])
-//				}
-//			}
-//			console.log($scope.arrherosort)
-			
-			$scope.he_isActive0=false;
-			$scope.he_isActive=true;
+			$scope.arrHeros=res.data;
 			$scope.herosort=function(evenHeros){
 				$scope.evensort=evenHeros;
-				$scope.he_isActive0=!$scope.he_isActive0
-				$scope.he_isActive=!$scope.he_isActive
 			}
 		})
 	}])
@@ -73,21 +56,66 @@ angular.module("wzry",['ngWaterfall','ui.router','angularCSS','homeModule','matc
 	.controller('communitycontrol',['$scope','$http',function($scope,$http){
 		$http.get("components/community/con_json/con_menu.json").success(function(res){
 			$scope.arrMenulist=res.rsp_obj.QueryForum_1.data.child_forums
+			/*默认加载项*/
+			$http.get("components/community/con_json/con1.json").success(function(res){
+					$scope.arrConText=res.rsp_obj.QueryTopic_1.data.pageContent;
+			})
 			$scope.menuname=$scope.arrMenulist[0].forum_title
 			$scope.menuShow=false;
-			$scope.addmenu=function(menuname){
+			
+			$scope.addmenu=function(menuname,i){
 				$scope.menuname=menuname
 				$scope.menuShow=false;
+				$http.get("components/community/con_json/con"+(i+1)+".json").success(function(res){
+					$scope.arrConText=res.rsp_obj.QueryTopic_1.data.pageContent;
+//					console.log($('#con_ul>li').last().offset().top)
+				})
 			}
+			
 			$scope.menushow=function(){
 				$scope.menuShow=!$scope.menuShow;
-			}
+			}	
+			
 		})
-		$http.get("components/community/con_json/con1.json").success(function(res){
-			$scope.arrConText=res.rsp_obj.QueryTopTopic_1.data.pageContent;
-			console.log(res.rsp_obj.QueryTopTopic_1.data.pageContent)
-		})
+		
+		/*瀑布流》》》》》》>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
+//		$scope.page=1 ,loading=true;
+//		$scope.arrConP = [];
+//		function pushContent() {                    //核心是这个函数，向$scope.posts
+
+                                                //添加内容
+//      if (loading) {                         //如果页面没有正在读取
+//          loading = false;                     //告知正在读取
+//          $http.get("components/community/con_json/con1.json") //调用API，读取第几页的内容
+//              .success(function (data) {
+//                  $scope.arrConold=data.rsp_obj.QueryForum_1.data.child_forums
+//                  for (var i = 0; i <= $scope.arrConold.length - 1; i++) {
+//                      $scope.arrConP.push($scope.arrConold[i]);
+//                  }
+////                  $timeout(function () {
+////                     var  main=document.querySelector('.main');
+////                      var today=Echo(main,document);//调用函数返回的对象
+////                     today.init({offset: 200, throttle: 10});
+////                  },10);
+//                  loading = true;            //告知读取结束
+//             });
+//	            page++;                             //翻页
+//	            if(page>4){
+//	                page=1;
+//	            }
+//      	}
+//      }
+//		
+//		  /*滚动事件监听*/
+//		$('#con_ul').on('scroll', function (event) {   //jquery，事件滚动监听
+//	        // if ($(document).scrollTop() + $(window).height() >= $(document).height()) {
+//	        if($('#con_ul>li').last().offset().top<$(document).height()){
+//	            //当滚动到页面底部
+//	            pushContent();                      //调用向$scope.posts添加内容函数
+//	        }
+//	    });	
 	}])
+	
 	
 	
 /*4.首页-论坛*/
